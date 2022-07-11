9d799b918ee468fd789cc2a33009234cdd1d154d
usernotavailable123 <102803913+usernotavailable123@users.noreply.github.com>
2022-07-11 22:54:36 +0800

    creating routes & schema for notes & users

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,9 @@ connectToMongo();
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello Sita!')
-})
+//Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
