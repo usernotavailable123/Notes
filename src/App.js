8202347fe43498670006ed266b75110ba0024430
usernotavailable123 <102803913+usernotavailable123@users.noreply.github.com>
2022-07-18 22:05:23 +0800

    set up router & links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-
+import {
+  BrowserRouter ,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <div className="App">
-      <h1>This is my react app</h1>
-    </div>
+      <BrowserRouter>
+      <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
